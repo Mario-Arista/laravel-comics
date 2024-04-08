f8034c45609d40ad30c1c2db795403ce9b2deb04
mario.arista <mario.arista.dev@gmail.com>
2024-04-08 17:21:30 +0200

    basic app.blade.php build with partials (solved a problem with partials )

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,15 @@
 @extends('layouts/app')
+
+@section('title')
+    DC COMICS
+@endsection
+
+@section('content')
+
+    <main>
+
+        <h2>main</h2>
+
+    </main>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,16 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> @yield('title')</title>
 
     <!-- Includiamo gli assets con la direttiva vite -->
-    @vite('resources/js/app.js');
+    @vite('resources/js/app.js')
 
 </head>
 
 <body>
-    <header>
-        <h1>ciao</h1>
-    </header>
+    @include('partials/header')
+
+    @yield('content')
+
+    @include('partials/footer')
+
+
+
 </body>
 </html>
