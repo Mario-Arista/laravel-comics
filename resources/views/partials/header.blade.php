e3c74ae1a256754e704a5fabaf794458c5d98ac1
mario.arista <mario.arista.dev@gmail.com>
2024-04-08 18:34:54 +0200

    Header almost done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,29 @@
 <header>
-    <h1>Ciao!</h1>
+    <div class="blue-header-element">
+        <div class="container">
+            <div class="text">DC power Visa</div>
+            <div class="text">Additional DC Sites</div>
+        </div>
+    </div>
+    <nav>
+        <div class="container">
+            <img class="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC">
+            <ul>
+                <li>Characters</li>
+                <li class="active">Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+            <div>
+                <input class="header-search" type="text" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </nav>
 </header>
