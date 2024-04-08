f7de3e6c874669d5f371202a187c2562a3a10a24
mario.arista <mario.arista.dev@gmail.com>
2024-04-08 19:29:53 +0200

    Part of the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,99 @@
 <footer>
-    Footer
+
+    {{-- TOP FOOTER --}}
+    <div id="top-footer">
+
+        <div class="container">
+    
+          <div id="link-row">
+    
+          <!-- Prima riga di link -->
+            <div id="link-row-first" class="links-row">
+              <div class="links-list">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li>Character</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>Games</li>
+                        <li>News</li>
+                    </ul>
+              </div>
+              <div class="links-list">
+                <h3>SHOP</h3>
+                <ul>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collection</li>
+                </ul>
+          </div>
+            </div>
+    
+            <!-- Seconda riga di link -->
+            <div id="link-row-second" class="links-row">
+              <div class="links-list">
+                    <h3>DC</h3>
+                    <ul>
+                        <li>Terms of Use</li>
+                        <li>Privacy policy</li>
+                        <li>Ad Choice</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+
+                    </ul>
+              </div>
+
+              <div class="links-list">
+                <h3>SITES</h3>
+                <ul>
+                    <li>DC</li>
+                    <li>MAD MAgazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+
+                </ul>
+          </div>
+            </div>
+    
+          </div>
+    
+          <!-- Immagine del logo -->
+          <div id="img-row">
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="DC logo">
+          </div>
+    
+        </div>
+
+    </div>
+
+
+    {{-- BOTTOM FOOTER --}}
+    <div id="bottom-footer">
+
+        <div class="container">
+    
+          <button>SIGN-UP NOW!</button>
+    
+            <div class="social-icons">
+                <span>FOLLOW US</span>
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook icon">
+                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter icon">
+                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube icon">
+                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Footer icon">
+                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope icon">
+            </div>
+            
+        </div>
+    
+    </div>
+
 </footer>
+
