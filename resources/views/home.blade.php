f55618315f70661f9220e069c40a2c725575c5da
mario.arista <mario.arista.dev@gmail.com>
2024-04-08 22:02:09 +0200

    fixing main + adding features

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,6 +24,44 @@
 
         </div>
 
+        <div id="app-features">
+
+            <div class="container">
+              <div class="single-feature">
+                    <div class="img-container">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="Digital Comics">
+                    </div>
+                
+                    <h3>DIGITAL COMICS</h3>
+              </div>
+
+              <div class="single-feature">
+                    <div class="img-container">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="Digital Merchandising">
+                    </div>
+                
+                    <h3>DC MERCHANDISING</h3>
+              </div>
+
+                <div class="single-feature">
+                    <div class="img-container">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="Comic Shop Locator">
+                    </div>
+            
+                    <h3>COMIC SHOP LOCATOR</h3>
+                </div>
+                
+                <div class="single-feature">
+                    <div class="img-container">
+                        <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="DC Power Visa">
+                    </div>
+            
+                    <h3>DC POWER VISA</h3>
+                </div>
+            </div>
+            
+          </div>
+
 
     </main>
 
